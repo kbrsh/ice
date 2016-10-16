2e6b1f2941895e32884a538b65ec00f91509ac4d
Gopal Shah <simplecooldude1@gmail.com>
2016-10-16 11:00:38 -0700

    data.js for getting data

---
+++ data.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+
+var options = {
+  host: 'quote.machinu.net',
+  path: '/api'
+};
+
+var quotes = [];
+
+function getData() {
+  setInterval(function () {
+    http.get(options, function(response){
+      var body = '';
+
+      response.on('data', function(chunk) {
+        body += chunk;
+      });
+
+      response.on('end', function(data){
+        data = JSON.parse(body);
+        console.log(data.text)
+        quotes.push(data.text)
+      });
+    }).on("error", function(e){
+      console.log("Got error: " + e.message);
+    });
+  }, 1500);
+}
+
+function pushData(cb) {
+  quotes = quotes.filter(function(item, pos) {
+    return quotes.indexOf(item) == pos;
+  });
+  fs.writeFile('data.json', JSON.stringify(quotes), function (err) {
+    console.log('Data inserted in data.json');
+    cb();
+  });
+}
+
+process.on('SIGINT', function() {
+    pushData(function() {
+      process.exit();
+    });
+});
