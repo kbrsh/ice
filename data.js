a87a271ba6dc88e702086a7f00096588448ad70e
Gopal Shah <simplecooldude1@gmail.com>
2016-10-17 16:59:26 -0700

    no key

--- data.js
+++ data.js
@@ -26,7 +26,7 @@ module.exports.addData = function(data) {
 
 
 function mineData() {
-  request('http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en', function (error, response, body) {
+  request('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function (error, response, body) {
       if (!error && response.statusCode == 200 && !body.match(new RegExp("\'", "g")) && !body.match(new RegExp("\"\"", "g"))) {
         var obj = JSON.parse(body);
         console.log(obj.quoteText);
