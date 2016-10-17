1729b7feb9751cbc2265f335c08a32e9f46ac713
Gopal Shah <simplecooldude1@gmail.com>
2016-10-16 21:37:05 -0700

    new

--- data.js
+++ data.js
@@ -27,15 +27,22 @@ module.exports.addData = function(data) {
 
 function mineData() {
   request('http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en', function (error, response, body) {
-      if (!error && response.statusCode == 200 && !body.match(new RegExp("\'", "g"))) {
+      if (!error && response.statusCode == 200 && !body.match(new RegExp("\'", "g")) && !body.match(new RegExp("\"\"", "g"))) {
         var obj = JSON.parse(body);
         console.log(obj.quoteText);
         quotes.push(obj.quoteText);
       }
   });
+  request('http://quote.machinu.net/api', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var obj = JSON.parse(body);
+        console.log(obj.text);
+        quotes.push(obj.text);
+      }
+  });
 }
 
-// setInterval(mineData, 1500);
+setInterval(mineData, 1500);
 
 
 process.on('SIGINT', function() {
