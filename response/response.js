8372f0f5bff7e0ae66908d7c6f3c2f4f5abdb6fb
Gopal Shah <simplecooldude1@gmail.com>
2016-10-17 19:55:10 -0700

    get

---
+++ response/response.js
@@ -0,0 +1,39 @@
+module.exports.get = function(data) {
+  var terminals = {};
+  var startwords = [];
+  var wordstats = {};
+
+  for (var i = 0; i < data.length; i++) {
+      var words = data[i].split(' ');
+      terminals[words[words.length-1]] = true;
+      startwords.push(words[0]);
+      for (var j = 0; j < words.length - 1; j++) {
+          if (wordstats.hasOwnProperty(words[j])) {
+              wordstats[words[j]].push(words[j+1]);
+          } else {
+              wordstats[words[j]] = [words[j+1]];
+          }
+      }
+  }
+
+  var choice = function (a) {
+      var i = Math.floor(a.length * Math.random());
+      return a[i];
+  };
+
+  var generate = function (min_length) {
+      word = choice(startwords);
+      var title = [word];
+      while (wordstats.hasOwnProperty(word)) {
+          var next_words = wordstats[word];
+          word = choice(next_words);
+          title.push(word);
+          if (title.length > min_length && terminals.hasOwnProperty(word)) break;
+      }
+      if (title.length < min_length || title.join(" ").length > 140 || title[title.length - 1].slice(-1) !== ("." || "!" || "?")) {
+        return generate(min_length);
+      } else {
+        return title.join(' ');
+      }
+  };
+}
