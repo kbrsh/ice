0d6994e753b2ebb51117e7e9718ab706e0fe3ccf
Kabir Shah <iamkbrsh@gmail.com>
2017-11-21 14:07:44 -0800

    refactor: range to hsl

--- index.py
+++ index.py
@@ -126,10 +126,7 @@ def rgb(c1, c2, c3):
     return ((c1 * 127.5) + 127.5, (c2 * 127.5) + 127.5, (c3 * 127.5) + 127.5)
 
 def hsl(c1, c2, c3):
-    c1 = (c1 * 0.5) + 0.5
-    c2 = (c2 * 0.5) + 0.5
-    c3 = (c3 * 0.5) + 0.5
-    (r, g, b) = colorsys.hls_to_rgb(c1, c3, c2)
+    (r, g, b) = colorsys.hls_to_rgb((c1 * 0.5) + 0.5, (c3 * 0.5) + 0.5, (c2 * 0.5) + 0.5)
     return (r * 255.0, g * 255.0, b * 255.0)
 
 colors = [rgb, hsl]
