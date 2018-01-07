021c49ac39ff96380fdae26ef8af431d654421c3
Kabir Shah <iamkbrsh@gmail.com>
2018-01-06 16:34:36 -0800

    Ice Transform

---
+++ transform.py
@@ -0,0 +1,57 @@
+import png
+import src.seed
+import src.pixel
+
+# Intro
+print("\x1b[36mIce\x1b[0m Transforming Image 💡")
+
+# Seed
+seedText = src.seed.generateSeed()
+
+# Read
+f = open("image.png", "rb")
+r = png.Reader(f)
+info = r.read()
+width = info[0]
+height = info[1]
+data = list(info[2])
+alpha = info[3]["alpha"]
+skip = 4 if alpha else 3
+f.close()
+
+# Transform
+transformSlope = 2.0 / 255.0
+transformYInt = -1.0
+pixel = src.pixel.generatePixel()
+pixelOperationC1 = pixel[0]
+pixelOperationC2 = pixel[1]
+pixelOperationC3 = pixel[2]
+pixelColor = pixel[3]
+
+for rowIndex, row in enumerate(data):
+    row = list(map(float, row))
+    for colIndex in range(0, len(row), skip):
+        r = (row[colIndex] * transformSlope) + transformYInt
+        r = pixelOperationC1.compute(r, -r)
+
+        g = (row[colIndex + 1] * transformSlope) + transformYInt
+        g = pixelOperationC2.compute(g, -g)
+
+        b = (row[colIndex + 2] * transformSlope) + transformYInt
+        b = pixelOperationC3.compute(b, -b)
+
+        r, g, b = pixelColor(r, g, b)
+        row[colIndex] = r
+        row[colIndex + 1] = g
+        row[colIndex + 2] = b
+
+    data[rowIndex] = row
+
+# Write
+f = open("transform.png", "wb")
+w = png.Writer(width, height, alpha=alpha)
+w.write(f, data)
+f.close()
+
+# Success
+print("\x1b[36mIce\x1b[0m Success \"" + seedText + "\" ✨")
