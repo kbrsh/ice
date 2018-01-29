51559f32ef4b3c53194d4e4cb69dd5b17579b1ab
Kabir Shah <iamkbrsh@gmail.com>
2018-01-28 21:55:02 -0800

    add slash noise generator

--- src/random/__init__.py
+++ src/random/__init__.py
@@ -13,11 +13,16 @@ def slash(key):
 
 # Random
 seed = (slash(ord('I')) + slash(ord('c')) + slash(ord('e'))) / 3
+frequency = 10.0
+period = 100
+noiseValues = []
 maximumSlopePixel = 2.0 / float(0xFFFFFFFFFFFFFFFF)
 maximumSlopeConstant = 14.0 / float(0xFFFFFFFFFFFFFFFF)
 
 def seedRandom(newSeed):
     global seed
+    for i in range(period):
+        noiseValues.append(float(random()) / float(0xFFFFFFFFFFFFFFFF))
     seed = newSeed
 
 def random():
@@ -27,3 +32,21 @@ def random():
 
 def randomConstant():
     return (maximumSlopeConstant * float(random())) - 7.0
+
+def noise(x):
+    # Frequency
+    x = x * frequency
+
+    # Floor to integer
+    xi = int(x)
+
+    # Find output values
+    xp = xi % period
+    ya = noiseValues[xp]
+    yb = noiseValues[xp if xp == period - 1 else xp + 1]
+
+    # Map x -> [0, 1]
+    x = x - xi
+
+    # Smoothly map x : [0, 1] -> [ya, yb]
+    return (x * x * x * (x * (x * (6.0 * yb - 6.0 * ya) + (15.0 * ya - 15.0 * yb)) + (10.0 * yb - 10.0 * ya)) + ya)
