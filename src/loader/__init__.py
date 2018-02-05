a28e25e8f59e1cb886434cbe678ffe580303a8f0
Kabir Shah <iamkbrsh@gmail.com>
2018-02-04 17:15:25 -0800

    longer loader

--- src/loader/__init__.py
+++ src/loader/__init__.py
@@ -1,5 +1,5 @@
 # Progress Loader
 def load(percent):
-    step = round(percent * 20.0)
+    step = round(percent * 25.0)
     end = " ⚡️\n" if percent == 1.0 else ""
     print("\r\x1b[36mIce\x1b[0m Processing [" + ("=" * step) + (" " * (20 - step)) + "] " + "{0:04.1f}%".format(percent * 100.0), end=end)
