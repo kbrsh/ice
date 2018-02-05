48ea10d2dbd8475e634e2872ecef169d12f440e6
Kabir Shah <iamkbrsh@gmail.com>
2018-02-04 21:34:39 -0800

    update loading bar size

--- src/loader/__init__.py
+++ src/loader/__init__.py
@@ -2,4 +2,4 @@
 def load(percent):
     step = round(percent * 25.0)
     end = " ⚡️\n" if percent == 1.0 else ""
-    print("\r\x1b[36mIce\x1b[0m Processing [" + ("=" * step) + (" " * (20 - step)) + "] " + "{0:04.1f}%".format(percent * 100.0), end=end)
+    print("\r\x1b[36mIce\x1b[0m Processing [" + ("=" * step) + (" " * (25 - step)) + "] " + "{0:04.1f}%".format(percent * 100.0), end=end)
