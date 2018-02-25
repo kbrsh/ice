b88f06b5b3b7de3c0d0618c69b677091bc936695
Kabir Shah <iamkbrsh@gmail.com>
2018-02-24 22:20:52 -0800

    add color generator

--- src/color/__init__.py
+++ src/color/__init__.py
@@ -1,4 +1,5 @@
 import colorsys
+from ..random import random
 
 # RGB
 def rgb(c1, c2, c3):
@@ -8,6 +9,11 @@ def rgb(c1, c2, c3):
 def hsl(c1, c2, c3):
     (r, g, b) = colorsys.hls_to_rgb((c1 * 0.5) + 0.5, (c3 * 0.5) + 0.5, (c2 * 0.5) + 0.5)
     return (r * 255.0, g * 255.0, b * 255.0)
+
+# Generate
+def generateColors():
+    color1 = float(random()) / float(0xFFFFFFFFFFFFFFFF)
+    return color1, (color1 + (float(random()) / (7.0 * float(0xFFFFFFFFFFFFFFFF)))) % 1.0
 
 colors = [rgb, hsl]
 colorsLength = len(colors)
