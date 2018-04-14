a62df5c368d142b41719160aa921509bb8455010
Kabir Shah <iamkbrsh@gmail.com>
2018-04-14 12:43:00 -0700

    descriptive comments

--- src/color/__init__.py
+++ src/color/__init__.py
@@ -10,7 +10,7 @@ def hsl(c1, c2, c3):
     (r, g, b) = colorsys.hls_to_rgb((c1 * 0.5) + 0.5, (c3 * 0.5) + 0.5, (c2 * 0.5) + 0.5)
     return (r * 255.0, g * 255.0, b * 255.0)
 
-# Generate
+# Generate colors
 def generateColors():
     color1 = float(random()) / float(0xFFFFFFFFFFFFFFFF)
     color2 = color1 + (1.0 / 7.0)
